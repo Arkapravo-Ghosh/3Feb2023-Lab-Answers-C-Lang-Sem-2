3dfbc52a26a195b9e8f9d8f79f8920c0fa21293d
Debojit <debojitganguly69@gmail.com>
2023-02-05 12:17:44 +0530

    Adding and deleting files

--- Lab3-Answers/Lab3-5.c
+++ Lab3-Answers/Lab3-5.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 #include <math.h>
-// Calculte the slope of a line
+// p q r s t u, output: area of triangle whose end points are p q, rs and tu
 void main() {
-    double a, b;
-    printf("Enter value of a: ");
-    scanf("%lf", &a);
-    printf("Enter value of b: ");
-    scanf("%lf", &b);
-
+    int p, q, r, s, t, u;
+    printf("Enter value of p: ");
+    scanf("%d", &p);
+    printf("Enter value of q: ");
+    scanf("%d", &q);
+    printf("Enter value of r: ");
+    scanf("%d", &r);
+    printf("Enter value of s: ");
+    scanf("%d", &s);
+    printf("Enter value of t: ");
+    scanf("%d", &t);
+    printf("Enter value of u: ");
+    scanf("%d", &u);
+    double pqrs = sqrt(pow((p - r), 2) + pow((q - s), 2));
+    double rstu = sqrt(pow((r - t), 2) + pow((s - u), 2));
+    double tupq = sqrt(pow((t - p), 2) + pow((u - q), 2));
+    double se = (pqrs + rstu + tupq) / 2;
+    printf("Area: %.*lf\n", 3, sqrt(se * (se - pqrs) * (se - rstu) * (se - tupq)));
 }
--- Lab3-Answers/Lab3-6.c
+++ Lab3-Answers/Lab3-6.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
 #include <math.h>
-// WAP which reads a b c d e and prints the distance between point a b and line cx + dy + e = 0
+// Calculte the slope of a line
 void main() {
-    double a, b, c, d, e;
+    double a, b;
     printf("Enter value of a: ");
     scanf("%lf", &a);
     printf("Enter value of b: ");
     scanf("%lf", &b);
-    printf("Enter value of c: ");
-    scanf("%lf", &c);
-    printf("Enter value of d: ");
-    scanf("%lf", &d);
-    printf("Enter value of e: ");
-    scanf("%lf", &e);
-    double f = (a * c + b * d + e) / sqrt(pow(c, 2) + pow(d, 2));
-    printf("Distance: %.3lf\n", f);
+
 }
