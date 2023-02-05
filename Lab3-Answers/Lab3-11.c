8ec829b05af26feabb894dcdfe3abdf016428771
Debojit <debojitganguly69@gmail.com>
2023-02-05 12:25:04 +0530

    Minor changes

--- Lab3-Answers/Lab3-11.c
+++ Lab3-Answers/Lab3-11.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<math.h>
 #define pi 3.14
-/*11.	Write program, which reads a, b and c as sides of a triangle and prints the angle A in degree. Hint: a2 = b2 +  c2 – 2bcCOS(A)*/
+/*Write program, which reads a, b and c as sides of a triangle and prints the angle A in degree. Hint: a2 = b2 +  c2 – 2bcCOS(A)*/
 int main(){
     int a,b,c;
     scanf("%d %d %d",&a,&b,&c);
